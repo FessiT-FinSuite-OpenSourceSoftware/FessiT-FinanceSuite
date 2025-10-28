2b29fe72750ed8215895f8be25cab9916563bd49
Vamsi Krishna Gurram <vamsi7632@gmail.com>
2025-10-28 10:38:00 +0530

    Added Notification Modal, with Infinite Scrolling, with Dummy Notification Data

--- finance-suite-professional-frontend/src/shared/SideBar/SideBar.jsx
+++ finance-suite-professional-frontend/src/shared/SideBar/SideBar.jsx
@@ -66,7 +66,7 @@ export default function SideBar({ component }) {
     }
   };
 
-  // Click outside to close modal (ignore bell clicks)
+ 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
